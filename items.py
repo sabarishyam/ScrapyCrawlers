b3c2b39823eac76b65b30ae87b4d8c6fa2cfb805
saba7349 <sabarishyam.Srinivas@rackspace.com>
2015-05-01 16:00:33 -0500

    Added a form post support for wetseal since html content is hidden

--- items.py
+++ items.py
@@ -18,3 +18,22 @@ class Apple(Item):
     #store_id = Field()
     #weekly_ad_url = Field()
     zipcode = Field()
+
+
+class Wetseal(Item):
+
+    city = Field()
+    address = Field()
+    store_country = Field()
+    store_services = Field()
+    store_hours = Field()
+    store_image_url = Field()
+    store_name = Field()
+    store_url = Field()
+    phone = Field()
+    state = Field()
+    store_email = Field()
+    store_floor_plan_url = Field()
+    store_id = Field()
+    weekly_ad_url = Field()
+    zipcode = Field()
