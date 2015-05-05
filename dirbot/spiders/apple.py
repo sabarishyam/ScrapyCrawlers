411c0fa5fdcb6bbd52cbe5b6bc1834ab3903e4f3
saba7349 <sabarishyam.Srinivas@rackspace.com>
2015-05-04 19:37:29 -0500

    Removed debug statements and added some comments to apple spider

--- dirbot/spiders/apple.py
+++ dirbot/spiders/apple.py
@@ -18,10 +18,14 @@ class Apple(Item):
     store_url = Field()
     phone = Field()
     state = Field()
+
+    #Couldnot find the following commented fields in the webpage
+
     #store_email = Field()
-    #store_floor_plan_url = Field()
+    #store_floor_plan_url = Field()    
     #store_id = Field()
     #weekly_ad_url = Field()
+
     zipcode = Field()
 
 class AppleSpider(CrawlSpider):
@@ -39,15 +43,20 @@ class AppleSpider(CrawlSpider):
         items = Apple()
 
         try:
+            #City
             items['city']  = sel.xpath('//span[@class="locality"]/text()').extract()[0]
+
+            #Address
             items['address'] = sel.xpath('//div[@class="street-address"]/text()').extract()[0]
 
+            #Store-Country
             store_country = urlparse.urlsplit(response.url).path.split('retail')
             if not store_country[0]:
                 items['store_country'] = 'us'
             else:
                 items['store_country'] = store_country[0][1:-1]
 
+            #Store Hours
             items['store_hours'] = {}
             store_hours = sel.xpath('//table[@class="store-info"]//tr//td').extract()[1:]
             day_of_week = store_hours[::2]
@@ -59,17 +68,27 @@ class AppleSpider(CrawlSpider):
                 index = day_of_week.index(day)
                 day = day.replace('<td>','').replace('</td>','')
                 day_hours[index] = day_hours[index].replace('<td>','').replace('</td>','')
-                items['store_hours'][day] = day_hours[index]
+                items['store_hours'][day] = day_hours[index].strip()
 
+            #Store Services
             items['store_services'] = sel.xpath('//div[@class="nav-buttons selfclear"]//a//@href').extract()
             
+            #Store Image Url
             items['store_image_url'] = sel.xpath('//div[@class="column last"]//img//@src').extract()[0]
+
+            #Store Url
             items['store_url'] = response.url
+
+            #Store Name
             items['store_name'] = sel.xpath('//div[@class="store-name"]/text()').extract()[0]
 
+            #Store Phone Number
             items['phone'] = sel.xpath('//div[@class="telephone-number"]/text()').extract()[0]
 
+            #Store State
             items['state'] = sel.xpath('//span[@class="region"]/text()').extract()[0]
+
+            #Store Zipcode
             items['zipcode'] = sel.xpath('//span[@class="postal-code"]/text()').extract()[0]
 
         except:
