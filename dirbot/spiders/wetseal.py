bd84a1d7f112b342ce873e8e5c40b56a8ec5e367
saba7349 <sabarishyam.Srinivas@rackspace.com>
2015-05-04 19:46:49 -0500

    sanitized wetseal spider

--- dirbot/spiders/wetseal.py
+++ dirbot/spiders/wetseal.py
@@ -14,19 +14,21 @@ class Wetseal(Item):
     city = Field()
     address = Field()
     store_country = Field()
-    #store_services = Field()
     store_hours = Field()
-    #store_image_url = Field()
     store_name = Field()
-    #store_url = Field()
     phone = Field()
     state = Field()
-    #store_email = Field()
-    #store_floor_plan_url = Field()
     store_id = Field()
-    #weekly_ad_url = Field()
     zipcode = Field()
 
+    #Couldnot find the following fields in tghe HTML page
+
+    #store_services = Field() 
+    #store_image_url = Field()
+    #store_url = Field()
+    #store_email = Field()
+    #store_floor_plan_url = Field()
+    #weekly_ad_url = Field()
 
 class WetsealSpider(Spider):
     name = "wetseal"
@@ -39,8 +41,6 @@ class WetsealSpider(Spider):
         form_url = sel.xpath('//form[@id="dwfrm_storelocator_state"]/@action').extract()
         states = sel.xpath('//option[@value]/@value').re(r'[A-Z]+')
 
-        #import pdb;pdb.set_trace()
-
         for state in states:            
             yield FormRequest(url=''.join(form_url),
                             formdata={'dwfrm_storelocator_address_states_stateUSCA': state,
@@ -50,40 +50,58 @@ class WetsealSpider(Spider):
     def parse1(self, response):
 
         sel = Selector(response)
-        #import pdb;pdb.set_trace()
 
-        #Needs cleansing of html 
+        #Store address selector
         store_address_list =  sel.xpath('//td[@class="store-address"]')
+
+        #Store name selector
         store_name_list = sel.xpath('//div[@class="store-name"]/text()').extract()
+
+        #Store hours selector
         store_hours_args = sel.xpath('//div[@class="store-hours"]')
+
+        #Fetch the Store state
         store_state = response.request.body.split('=')[-1]
 
         items = []
 
-        for address in store_address_list:
+        for index,address in enumerate(store_address_list):
             
             item = Wetseal()
-            index = store_address_list.index(address)
             _args = address.extract().replace('\t','').split('\n')[1:-1]
             
             store_hours = regex.sub('', store_hours_args[index].extract()).strip('<div class="store-hours">').split('<br><br>')
             item['store_hours'] = {}
 
-            # Need to revisit sanitizing strings of  : , <html_tags>
+            # Parse Store hours to match a dict
             for _entity in store_hours:
                 
                 #Check to prevent any residual html tag from polluting store_hours dict()
                 if not re.match('\w+', _entity):
                     break
                 _day,_hours = _entity.split(':',1)
-                item['store_hours'][_day] = _hours
+                item['store_hours'][_day] = _hours.replace('</','').strip()
             
+            #Store Phone number
             item['phone'] = _args[-1].strip()
-            item['address'] = ''.join(_args[:-1]).strip()            
+
+            #Store Address
+            item['address'] = ''.join(_args[:-1]).strip()
+
+            #Store Name         
             item['store_name'] = store_name_list[index].strip()
+
+            #Store State
             item['state'] = store_state
+
+            #Store Zipcode
             item['zipcode'] = _args[-2].split(' ')[-1].replace('<br>','')
+
+            #Store Country
+            #US because all the zipcodes were self-contained to USA
             item['store_country'] = 'US'
+
+            #Store City
             item['city'] =  _args[-2].split(',')[0].strip()
 
             items.append(item)
